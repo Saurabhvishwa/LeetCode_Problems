1eef7df1f3502775c1c391f6fc0af4a363bc26ae
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-04-13 09:25:29 +0530

    Time: 2 ms (98.70%), Space: 42.3 MB (83.19%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int threeSumMulti(int[] arr, int target) {
+        int mod = 1_000_000_007;
+        long result = 0;
+        long[] freq = new long[101];
+        for(int i:arr){
+            freq[i]++;
+        }
+        for(int i=0;i<=100;i++){
+            for(int j=i;j<=100;j++){
+                int k = target - i - j;
+                if(k<0 || k> 100){
+                    continue;
+                }
+                if(i == j && j == k){
+                    result += (freq[i]*(freq[i]-1)*(freq[i]-2))/6;
+                }else if(i == j && j!=k){
+                    result += (freq[i]*(freq[i]-1)/2)*freq[k];
+                }else if(i <j && j< k){
+                    result += freq[i]*freq[j]*freq[k];
+                }
+            }
+        }
+        return (int) (result%mod);
+    }
+}
