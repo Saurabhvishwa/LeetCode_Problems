b1a9c5b53438f2be6949ba1bc8bad1191d838ca5
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-04-21 09:46:25 +0530

    Time: 31 ms (10.26%), Space: 40 MB (73.86%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,22 +3,23 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int beg,last,mid;
-        beg = 1 ; last = n;
-       int pos = 1;
-        while(beg<=last){
+        int start = 1;
+        int end = n;
+        int middle;
+        int index = 1;
+        while(start<=end){
             // ensure you calculate mid values this way ,otherwise ,it would cause overflow
-            mid = beg + (last-beg)/2;
-            boolean x = isBadVersion(mid);
+            middle = start+ (end-start)/2;
+            boolean x = isBadVersion(middle);
             if(x == true){
-                pos = mid;
-                last = mid-1;
+                index = middle;
+                end = middle-1;
             }
             else
-                beg = mid+1;
+                start = middle+1;
         }
 
         // return the first index of faulty product
-        return pos;
+        return index;
     }
 }
