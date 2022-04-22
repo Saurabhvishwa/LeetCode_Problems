586c856dcd04b9d56d93f9d80faa97bc5f52b925
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-04-22 11:44:01 +0530

    Create DFS.java
    DFS

---
+++ Graph/DFS.java
@@ -0,0 +1,15 @@
+class DFS{
+
+  public ArrayList<Integer> getDFS(boolean[] visited, int root, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> list){
+        if(visited[root] == false){
+            list.add(root);
+            visited[root] = true;
+            ArrayList<Integer> neighbors = adj.get(root);
+            for(int el:neighbors){
+                list = getDFS(visited, el, adj, list);
+            }
+        }
+        return list;
+    }
+  
+}
