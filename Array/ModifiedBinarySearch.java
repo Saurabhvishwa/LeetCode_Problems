62dbcbb01d39c6cc68b043725b014cb505c4117a
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-04-23 12:00:07 +0530

    Create ModifiedBinarySearch.java
    Modified Binary Search

---
+++ Array/ModifiedBinarySearch.java
@@ -0,0 +1,27 @@
+class ModifiedBinarySearch{
+
+  public static int modifiedBinarySearch(int[] arr, int key){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid] == key) return mid;
+            else if(arr[start]>=arr[mid]){
+                if(key<=arr[start] && key>arr[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }else{
+                if(key<arr[mid] && key>arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        
+        }
+        return start;
+    }
+  
+}
