16cb645f29a7514a92d6053f71c6cb8145a63745
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-04-24 13:03:36 +0530

    Time: 13 ms (36.91%), Space: 42.7 MB (72.28%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordSegment(String s, List<String> dict){
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i=0;i<=s.length();i++){
+            if(dp[i]){
+                for(String str:dict){
+                    int end = i+str.length();
+                    if(end<=s.length()){
+                        if(str.equals(s.substring(i,end))){
+                            dp[end] = true;
+                        }
+                    }else continue;
+                }
+            }else continue;
+        System.out.println(Arrays.toString(dp));
+        }
+        return dp[s.length()];
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordSegment(s, wordDict);
+    }
+}
