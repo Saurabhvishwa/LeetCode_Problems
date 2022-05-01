f0e1799539f0a54d4de08de84eb16193667e8abd
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-01 15:33:35 +0530

    Time: 2 ms (57.60%), Space: 40.8 MB (77.65%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,36 +1,60 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-      Stack<Character> a = new Stack<>();
+    public boolean checkBS(String s, String t){
+        StringBuilder a = new StringBuilder("");
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '#'){
-                if(a.size() > 0){
-                    a.pop();
+                if(a.length()>0){
+                    a.replace(a.length()-1, a.length(),"");
                 }
             }else{
-                a.push(s.charAt(i));
+                a.append(String.valueOf(s.charAt(i)));
             }
         }
-         Stack<Character> b = new Stack<>();
+       StringBuilder b = new StringBuilder("");
         for(int i=0;i<t.length();i++){
             if(t.charAt(i) == '#'){
-                if(b.size() > 0){
-                    b.pop();
+                if(b.length()>0){
+                    b.replace(b.length()-1, b.length(),"");
                 }
             }else{
-                b.push(t.charAt(i));
+                b.append(String.valueOf(t.charAt(i)));
             }
         }
-        if(a.size() != b.size()) return false;
+        return a.toString().equals(b.toString());
+    }
+    public boolean backspaceCompare(String s, String t) {
+//       Stack<Character> a = new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i) == '#'){
+//                 if(a.size() > 0){
+//                     a.pop();
+//                 }
+//             }else{
+//                 a.push(s.charAt(i));
+//             }
+//         }
+//          Stack<Character> b = new Stack<>();
+//         for(int i=0;i<t.length();i++){
+//             if(t.charAt(i) == '#'){
+//                 if(b.size() > 0){
+//                     b.pop();
+//                 }
+//             }else{
+//                 b.push(t.charAt(i));
+//             }
+//         }
+//         if(a.size() != b.size()) return false;
         
-        while (!a.empty()){
-            if (a.peek() == b.peek()){
-                a.pop();
-                b.pop();
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
+//         while (!a.empty()){
+//             if (a.peek() == b.peek()){
+//                 a.pop();
+//                 b.pop();
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+        return checkBS(s, t);
+        
     }
 }
