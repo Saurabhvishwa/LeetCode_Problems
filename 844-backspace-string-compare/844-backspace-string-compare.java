56c838702d1efdee6f6d1c4685388dd938ff09dd
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-01 15:34:11 +0530

    Time: 3 ms (41.40%), Space: 42.1 MB (52.87%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean checkBS(String s, String t){
+    public boolean backspaceCompare(String s, String t) {
         StringBuilder a = new StringBuilder("");
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '#'){
@@ -21,40 +21,5 @@ class Solution {
             }
         }
         return a.toString().equals(b.toString());
-    }
-    public boolean backspaceCompare(String s, String t) {
-//       Stack<Character> a = new Stack<>();
-//         for(int i=0;i<s.length();i++){
-//             if(s.charAt(i) == '#'){
-//                 if(a.size() > 0){
-//                     a.pop();
-//                 }
-//             }else{
-//                 a.push(s.charAt(i));
-//             }
-//         }
-//          Stack<Character> b = new Stack<>();
-//         for(int i=0;i<t.length();i++){
-//             if(t.charAt(i) == '#'){
-//                 if(b.size() > 0){
-//                     b.pop();
-//                 }
-//             }else{
-//                 b.push(t.charAt(i));
-//             }
-//         }
-//         if(a.size() != b.size()) return false;
-        
-//         while (!a.empty()){
-//             if (a.peek() == b.peek()){
-//                 a.pop();
-//                 b.pop();
-//             }
-//             else{
-//                 return false;
-//             }
-//         }
-        return checkBS(s, t);
-        
     }
 }
