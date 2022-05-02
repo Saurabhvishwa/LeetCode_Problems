71de96436655b35a3c764b729d25183937d27554
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-02 10:14:16 +0530

    Time: 1 ms (91.32%), Space: 46.7 MB (59.41%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,34 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        int[] left = new int[height.length];
-        int[] right = new int[height.length];
-        Stack<Integer> stack = new Stack<>();
-        stack.push(height[0]);
-        left[0] = -1;
-        for(int i=1;i<height.length;i++){
-            if(stack.peek() > height[i]){
-                left[i] = stack.peek();
+        int result = 0;
+        int prevIndex = 0;
+        int size = height.length-1;
+        int prev = height[0];
+        
+        int temp = 0;
+        for(int i=1;i<=size;i++){
+            if(height[i] < prev){
+                result += prev - height[i];
+                temp += prev - height[i];
             }else{
-                stack.push(height[i]);
-                left[i] = -1;
-            }
-        }
-        right[right.length-1] = -1;
-            stack.clear();
-        stack.push(height[height.length-1]);
-        for(int i=right.length-2;i>=0;i--){
-            if(stack.peek() > height[i]){
-                right[i] = stack.peek();
-            }else{
-                stack.push(height[i]);
-                right[i] = -1;
+                prev = height[i];
+                prevIndex = i;
+                temp = 0;
             }
         }
         
-        int result = 0;
-        for(int i=0;i<height.length;i++){
-            if(left[i] != -1 && right[i] != -1){
-                result += Math.abs(Math.min(left[i], right[i]) - height[i]);
+        if(prevIndex < size){
+            result -= temp;
+            prev = height[size];
+            for(int i=size; i>=prevIndex;i--){
+                if(height[i]>=prev){
+                    prev = height[i];
+                }else{
+                    result += prev-height[i];
+                }
             }
         }
         return result;
