032c2299af8c669b3de15156161c1b32f381ee1d
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-03 10:09:44 +0530

    Time: 25 ms (5.51%), Space: 44.2 MB (68.30%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        int[][] dp = new int[2][word2.length()+1];
         dp[0][0] = 0;
         for(int i=0;i<=word1.length();i++){
             for(int j=0;j<=word2.length();j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = 0;
+                    dp[i%2][j] = 0;
                 }
                 else if(i == 0){
-                    dp[i][j] = j;
+                    dp[i%2][j] = j;
                 }else if(j == 0){
-                    dp[i][j] = i;
+                    dp[i%2][j] = i;
                 }else{
                     if(word1.charAt(i-1) == word2.charAt(j-1)){
-                        dp[i][j] = dp[i-1][j-1];
+                        dp[i%2][j] = dp[(i-1)%2][j-1];
                     }else{
-                        dp[i][j] = 1+Math.min(Math.min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]);
+                        dp[i%2][j] = 1+Math.min(Math.min(dp[(i-1)%2][j-1], dp[(i-1)%2][j]), dp[i%2][j-1]);
                     }
                 }
             }
         }
-        return dp[word1.length()][word2.length()];
+        for(int[] i:dp){
+            System.out.println(Arrays.toString(i));
+        }
+        return dp[word1.length()%2][word2.length()];
     }
 }
