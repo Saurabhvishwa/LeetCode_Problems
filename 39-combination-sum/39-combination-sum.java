9cef5de68ffa343a7e95de9b6bc9898e2af6b2c8
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-04 10:18:44 +0530

    Time: 13 ms (13.15%), Space: 44.6 MB (57.78%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public void getComb(List<List<Integer>> ans, List<Integer> temp, int[] arr, int index, int sum){
+        if(sum == 0){
+            ans.add(new ArrayList<>(temp));
+            return ;
+        }
+        for(int i=index;i<arr.length;i++){
+            if(sum - arr[i] >= 0){
+                temp.add(arr[i]);
+                getComb(ans, temp, arr, i, sum-arr[i]);
+                
+                temp.remove(temp.indexOf(arr[i]));
+            }
+        }
+        return ;
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> l = Arrays.stream(candidates).boxed().collect(Collectors.toList());
+        Set<Integer> set = new TreeSet<Integer>(l);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        getComb(ans, temp, candidates, 0, target);
+        return ans;
+        
+    }
+}
