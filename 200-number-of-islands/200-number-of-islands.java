3a78ffdbb69f5f44d0860c0ded4bb8bc1365db16
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-06 11:33:39 +0530

    Time: 7 ms (29.39%), Space: 56.6 MB (62.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,15 +1,15 @@
 class Solution {
-    public boolean safePosition(char[][] grid, int row, int col, boolean[][] visited){
-        if((row>=0 && row<grid.length) && (col>=0 && col<grid[0].length) && visited[row][col] == false && (grid[row][col] == '1')) return true;
+    public boolean safePosition(char[][] grid, int row, int col){
+        if((row>=0 && row<grid.length) && (col>=0 && col<grid[0].length) && (grid[row][col] == '1')) return true;
         return false;
     }
-    public void DFS(char[][] grid, int row, int col, boolean[][] visited){
+    public void DFS(char[][] grid, int row, int col){
         int[] ROW = {0, 1, 0, -1};
         int[] COL = {1, 0, -1, 0};
-        visited[row][col] = true;
+        grid[row][col] = '0';
         for(int i=0;i<4;i++){
-            if(safePosition(grid, row+ROW[i], col+COL[i], visited)){
-                DFS(grid, row+ROW[i], col+COL[i], visited);
+            if(safePosition(grid, row+ROW[i], col+COL[i])){
+                DFS(grid, row+ROW[i], col+COL[i]);
             }
         }
     }
@@ -17,11 +17,10 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
-        boolean[][] visited = new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
-                    DFS(grid, i, j, visited);
+                if(grid[i][j] == '1'){
+                    DFS(grid, i, j);
                     count++;
                 }
             }
