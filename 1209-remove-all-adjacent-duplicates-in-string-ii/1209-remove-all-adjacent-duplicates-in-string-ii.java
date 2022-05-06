e2d7d3e3104f3b231b8835eb793012b8c612c8d5
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-06 10:49:59 +0530

    Time: 21 ms (84.99%), Space: 43.1 MB (85.92%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,14 +1,14 @@
 class Solution {
-    public String removeDuplicates(String S, int K) {
-        char[] SC = S.toCharArray();
-        int i, j;
-        Stack<Integer> st = new Stack<>();
-        st.add(0);
-        for (i = 1, j = 1; j < S.length(); i++, j++) {
-            char chr = SC[i] = SC[j];
-            if (i == 0 || chr != SC[i-1]) st.add(i);
-            else if (i - st.peek() + 1 == K) i = st.pop() - 1;
+    public String removeDuplicates(String S, int k) {
+        char[] arr = S.toCharArray();
+            Stack<Integer> st = new Stack<>();
+        int i,j;
+        st.push(0);
+        for(i=1,j=1;j<arr.length;i++,j++){
+            char temp = arr[i] = arr[j];
+            if(i == 0 || temp != arr[i-1]) st.push(i);
+            else if( i - st.peek() +1 == k) i = st.pop()-1;
         }
-        return new String(SC, 0, i);
+        return new String(arr, 0, i);
     }
 }
