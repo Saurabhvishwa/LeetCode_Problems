20fcd1d1e833743895dfaaae3f35d25dde7eb9ff
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-07 09:41:10 +0530

    Time: 150 ms (5.99%), Space: 96.9 MB (5.05%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
         int n = nums.length;
-        int[] min = new int[n];
-        min[0] = nums[0];
-        for(int i=1;i<n;i++){
-            min[i] = Math.min(min[i-1], nums[i]);
+        st.push(nums[0]);
+        for(int i=1;i<n-1;i++){
+            st.push(Math.min(st.peek(), nums[i]));
         }
         TreeSet<Integer> tree = new TreeSet<>();
         for(int i=n-1;i>=0;i--){
             int num = nums[i];
             Integer right = tree.lower(num);
+            if(st.isEmpty()) return false;
+            int left = st.pop();
             if(i>0 && right != null){
-                int left = min[i-1];
                 if(left < right) return true;
             }
             tree.add(num);
         }
-        System.out.println(tree);
         return false;
     }
 }
