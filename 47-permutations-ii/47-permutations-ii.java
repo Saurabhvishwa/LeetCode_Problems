386c2e052449c11dfe9b06b84da8d8619cbd5c3a
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-11 12:02:18 +0530

    Time: 213 ms (7.86%), Space: 63.4 MB (5.49%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,8 +1,23 @@
 class Solution {
     public boolean isPermutable(int[] nums, int start, int curr){
-        for(int i=start;i<curr;i++){
-            if(nums[i] == nums[curr]){
+            // for(int i=start;i<curr;i++){
+            //     if(nums[i] == nums[curr]){
+            //         return false;
+            //     }
+            // }
+        
+        int end = curr-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid] == nums[curr]){
+            
+                System.out.println(Arrays.toString(nums)+" "+start+" "+curr);
                 return false;
+            }
+            else if(nums[mid] > nums[curr]){
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
         return true;
@@ -12,12 +27,11 @@ class Solution {
             ans.add(new ArrayList<>(temp));
             return;
         }
-        
         for(int i=0;i<nums.length;i++){
-            if(isPermutable(nums, 0, i) && nums[i] != Integer.MAX_VALUE){
+            if(isPermutable(nums, 0, i) && nums[i] != Integer.MIN_VALUE){
                 temp.add(nums[i]);
                 int value = nums[i];
-                nums[i] = Integer.MAX_VALUE;
+                nums[i] = Integer.MIN_VALUE;
                 perm(nums, ans, temp);
                 nums[i] = value;
                 temp.remove(temp.size()-1);
@@ -28,6 +42,7 @@ class Solution {
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
+        
         List<List<Integer>> ans = new ArrayList<>();
         perm(nums , ans, new ArrayList<Integer>());
         return ans;
