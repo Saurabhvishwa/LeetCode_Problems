6a576e7b533660ee90959fc3240e18c4debf2ffb
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-11 11:18:01 +0530

    Time: 5 ms (10.50%), Space: 45.1 MB (23.92%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,14 +1,16 @@
 class Solution {
-    public void permutation(int[] nums, int index, List<List<Integer>> ans, List<Integer> temp){
+    public void permutation(int[] nums, int index, List<List<Integer>> ans, List<Integer> temp, Map<Integer, Integer> visited){
         if(temp.size() == nums.length){
             ans.add(new ArrayList<>(temp));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(!temp.contains(nums[i])){
+            if(!visited.containsKey(nums[i])){
                 temp.add(nums[i]);
-                permutation(nums, i+1, ans, temp);
+                visited.put(nums[i], 1);
+                permutation(nums, i+1, ans, temp, visited);
                 temp.remove(temp.size()-1);
+                visited.remove(nums[i]);
                 
             }
         }
@@ -16,7 +18,7 @@ class Solution {
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        permutation(nums, 0, ans, new ArrayList<Integer>());
+        permutation(nums, 0, ans, new ArrayList<Integer>(), new HashMap<Integer, Integer>());
         return ans;
     }
 }
