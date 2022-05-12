3700709d913da386681cee3125e59da1cd9066c8
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-12 09:59:52 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (87.11%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,4 +1,13 @@
 class Solution {
+    public double pow(double x, int n){
+        if(n==0) return 1;
+        if(n==1) return x;
+        double ans = pow(x, n/2);
+        if(n%2==0){
+            return ans*ans;
+        }
+        return ans*ans*x;
+    }
     public double power(double x, int n) {
         if(n == Integer.MAX_VALUE) return x;
         else if(n == Integer.MIN_VALUE) return (x==1||x==-1) ? 1:0;
@@ -6,11 +15,7 @@ class Solution {
             x = 1/x;
             n *= -1;
         }
-        double ans = 1;
-        for(int i=0;i<n;i++){
-            ans = ans*x;
-        }
-        return ans;
+        return pow(x, n);
     }
     public double myPow(double x, int n) {
         return power(x, n);
