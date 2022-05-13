6f2cbf887f708b5a366339e627114f63b0b293c9
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-13 10:09:24 +0530

    Time: 70 ms (8.31%), Space: 47 MB (7.27%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public void joinNext(Node root){
+        if(root == null) return;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        while(!queue.isEmpty()){
+            Node current = queue.poll();
+            if(current!=null){
+                    if(current.left != null) queue.add(current.left);
+                    if(current.right != null) queue.add(current.right);
+                
+                    while(!queue.isEmpty() && queue.peek() != null){
+                    Node next = queue.poll();
+                    System.out.println(current.val+" "+next.val);
+                    current.next = next;
+                    current = next;
+                    if(current.left != null) queue.add(current.left);
+                    if(current.right != null) queue.add(current.right);
+                    }
+                    queue.add(null);
+                    if(!queue.isEmpty()) queue.poll();
+            }
+            
+            
+        }
+    }
+    public Node connect(Node root) {
+        joinNext(root);
+        return root;
+    }
+}
