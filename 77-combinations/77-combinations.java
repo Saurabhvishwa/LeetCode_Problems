b1db6c1b67c5054e9b1f218681ac33b767ab7311
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-18 19:13:13 +0530

    Time: 6 ms (88.55%), Space: 56.8 MB (20.42%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,18 +1,19 @@
 class Solution {
-    public void comb(int index, int n, int k, LinkedList<Integer> temp, List<List<Integer>> ans){
+    List<List<Integer>> ans;
+    public void comb(int index, int n, int k, LinkedList<Integer> temp){
         if(k == 0){
             ans.add(new LinkedList<>(temp));
             return;
         }
         for(int i=index;i<=n-k+1;i++){
             temp.add(i);
-            comb(i+1, n, k-1, temp, ans);
+            comb(i+1, n, k-1, temp);
             temp.removeLast();
         }
     }
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans = new LinkedList<>();
-        comb(1, n, k, new LinkedList<Integer>(), ans);
+        ans = new LinkedList<>();
+        comb(1, n, k, new LinkedList<Integer>());
         return ans;
     }
 }
