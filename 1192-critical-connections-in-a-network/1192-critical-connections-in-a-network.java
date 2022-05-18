1ecb7663f313a42471b570e7055ca5d513d2b0ab
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-18 12:28:15 +0530

    Time: 216 ms (45.73%), Space: 310.6 MB (51.10%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public void dfs(Map<Integer, List<Integer>> graph, int current, int parent, int[] low, int[] disc, int time){
+        disc[current] = low[current] = time++;
+        for(Integer v : graph.get(current)){
+            if(disc[v] == 0){
+                dfs(graph, v, current, low, disc, time);
+                low[current] = Math.min(low[current], low[v]);
+                if(low[v] > disc[current]){
+                    ans.add(Arrays.asList(current, v));
+                }
+            }else if(v != parent){
+                low[current] = Math.min(low[current], disc[v]);
+            }
+        }
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        int[] low = new int[n], disc = new int[n];
+        int time = 1;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i=0;i<n;i++){
+            graph.put(i, new ArrayList<Integer>());
+        }
+        for(List<Integer> l : connections){
+            graph.get(l.get(0)).add(l.get(1));
+            graph.get(l.get(1)).add(l.get(0));
+        }
+        dfs(graph, 0, -1, low, disc, time);
+        return ans;
+    }
+}
