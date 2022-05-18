fbf19e1e9d53627bbda26ef657893e2356c9bdb3
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-18 19:49:24 +0530

    Time: 2 ms (28.07%), Space: 44.3 MB (7.42%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void subset(int[] nums, int index, int n, LinkedList<Integer> temp, List<List<Integer>> ans){
+        if(index>=n) return;
+        temp.add(nums[index]);
+        ans.add(new LinkedList<>(temp));
+        subset(nums, index+1, n, temp, ans);
+        temp.removeLast();
+        subset(nums, index+1, n, temp, ans);
+        }
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new LinkedList<>();
+        ans.add(new LinkedList<>());
+        int n = nums.length;
+        subset(nums, 0, n, new LinkedList<>(), ans);
+        return ans;
+    }
+}
