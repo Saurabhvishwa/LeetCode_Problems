fefb3561db75139d4c9f6a6a6eb3a5bc75801fd8
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-18 12:29:32 +0530

    Time: 148 ms (74.85%), Space: 99 MB (96.22%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,10 +1,9 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public void dfs(Map<Integer, List<Integer>> graph, int current, int parent, int[] low, int[] disc, int time){
+    public void dfs(Map<Integer, List<Integer>> graph, int current, int parent, int[] low, int[] disc, int time, List<List<Integer>> ans){
         disc[current] = low[current] = time++;
         for(Integer v : graph.get(current)){
             if(disc[v] == 0){
-                dfs(graph, v, current, low, disc, time);
+                dfs(graph, v, current, low, disc, time, ans);
                 low[current] = Math.min(low[current], low[v]);
                 if(low[v] > disc[current]){
                     ans.add(Arrays.asList(current, v));
@@ -15,8 +14,6 @@ class Solution {
         }
     }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int[] low = new int[n], disc = new int[n];
-        int time = 1;
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for(int i=0;i<n;i++){
             graph.put(i, new ArrayList<Integer>());
@@ -25,7 +22,10 @@ class Solution {
             graph.get(l.get(0)).add(l.get(1));
             graph.get(l.get(1)).add(l.get(0));
         }
-        dfs(graph, 0, -1, low, disc, time);
+        int[] low = new int[n], disc = new int[n];
+        int time = 1;
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(graph, 0, -1, low, disc, time, ans);
         return ans;
     }
 }
