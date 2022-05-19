4fb207847682112aee9ba393fbab882cf41cec15
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-19 09:54:18 +0530

    Time: 11 ms (63.84%), Space: 54.6 MB (20.69%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,18 +1,13 @@
 class Solution {
-    public boolean valid(int row, int col, int m, int n){
-        if(row<0 || row>=m || col<0 || col>=n) return false;
-        return true;
-    }
-    public int min(int[] arr){
+    public int max(int[] arr){
         int max = arr[0];
         for(int i=1;i<arr.length;i++){
             max = Math.max(max, arr[i]);
         }
         return max;
     }
-    
     public int longestPath(int[][] matrix, int row, int col, int[][] dp, int m, int n){
-        if(!valid(row, col, matrix.length, matrix[0].length)) return 0;
+        if(row<0 || row>=m || col<0 || col>=n) return 0;
         if(dp[row][col] != 0) return dp[row][col];
         int[] temp = new int[5];
         if(col<n-1 && matrix[row][col+1]>matrix[row][col]){
@@ -28,9 +23,10 @@ class Solution {
             temp[3] = dp[row][col] + 1 + longestPath(matrix, row-1, col, dp, m, n);
         }
         temp[4] = 1;
-        return dp[row][col] = min(temp);
+        return dp[row][col] = max(temp);
     }
-    public int path(int[][] matrix, int m, int n){
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
         int[][] dp = new int[m][n];
         int ans = Integer.MIN_VALUE;
         for(int i=0;i<m;i++){
@@ -42,9 +38,5 @@ class Solution {
             }
         }
         return ans;
-        
-    }
-    public int longestIncreasingPath(int[][] matrix) {
-        return path(matrix, matrix.length, matrix[0].length);
     }
 }
