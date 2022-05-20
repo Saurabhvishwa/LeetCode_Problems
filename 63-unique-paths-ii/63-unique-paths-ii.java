7af6ae49a21100c50cef2bab2fd71904a562de99
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-20 09:29:29 +0530

    Time: 1 ms (52.25%), Space: 42.5 MB (35.66%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,37 +3,23 @@ class Solution {
         if(grid[0][0] == 1) return 0;
         int m = grid.length;
         int n = grid[0].length;
-        if(grid[m-1][n-1] == 1) return 0;
+        grid[0][0] = 1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 1){
-                    grid[i][j] = -1;
+                if(i == 0 && j==0){
                     continue;
                 }
-                if(i == 0 && j == 0){
-                    grid[i][j] = 1;
+                if(grid[i][j] == 1){
+                    grid[i][j] = 0;
                 }else if(i == 0){
-                    if(grid[i][j-1] != -1){
-                        grid[i][j] = grid[i][j-1];
-                    }else{
-                        grid[i][j] = 0;
-                    }
-                }else if(j == 0){
-                    if(grid[i-1][j] != -1){
-                        grid[i][j] = grid[i-1][j];
-                    }else{
-                        grid[i][j] = 0;
-                    }
+                    grid[i][j] = grid[i][j-1];
+                }else if(j==0){
+                    grid[i][j] = grid[i-1][j];
                 }else{
-                    if(grid[i][j-1] != -1 && grid[i-1][j] != -1){
-                        grid[i][j] = grid[i-1][j] + grid[i][j-1];
-                    }else if(grid[i][j-1] != -1){
-                        grid[i][j] = grid[i][j-1];
-                    }else if(grid[i-1][j] != -1){
-                        grid[i][j] = grid[i-1][j];
-                    }
+                    grid[i][j] = grid[i-1][j]+grid[i][j-1];
                 }
             }
+            
         }
         return grid[m-1][n-1];
     }
