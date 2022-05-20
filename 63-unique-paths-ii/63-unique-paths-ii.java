85b53ee85fa3f0828a77b6e0c6008cd3be8d7c87
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-20 09:32:24 +0530

    Time: 1 ms (52.25%), Space: 41.9 MB (67.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,12 +1,11 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-        if(grid[0][0] == 1) return 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        grid[0][0] = 1;
+        int m = grid.length, n = grid[0].length;
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i == 0 && j==0){
+                    grid[i][j] = 1;
                     continue;
                 }
                 if(grid[i][j] == 1){
@@ -19,7 +18,6 @@ class Solution {
                     grid[i][j] = grid[i-1][j]+grid[i][j-1];
                 }
             }
-            
         }
         return grid[m-1][n-1];
     }
