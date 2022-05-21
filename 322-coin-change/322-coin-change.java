e8be4b30dde9a1efc5248eb3ef65e198788b2394
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-21 09:09:42 +0530

    Time: 36 ms (33.48%), Space: 44.8 MB (61.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,8 +2,6 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
         int[] dp = new int[amount+1];
-        // Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
         for(int i=1;i<=amount;i++){
             dp[i] = Integer.MAX_VALUE;
             for(int c : coins){
