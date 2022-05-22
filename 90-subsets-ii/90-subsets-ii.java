6e19ee38071286f356a1cc1f1cded9593ea7343f
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-22 09:46:15 +0530

    Time: 5 ms (15.89%), Space: 44.5 MB (16.87%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void subset(int[] nums, int index, List<List<Integer>> ans, LinkedList<Integer> temp, int n){
+        ans.add(new LinkedList<>(temp));
+        for(int i=index;i<n;i++){
+            if(i == index || nums[i] != nums[i-1]){
+                temp.add(nums[i]);
+                subset(nums, i+1, ans, temp, n);
+                temp.removeLast();
+            }
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new LinkedList<>();
+        subset(nums, 0, ans, new LinkedList<Integer>(), nums.length);
+        return ans;
+    }
+}
