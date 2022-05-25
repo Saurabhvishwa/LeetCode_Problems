7806034cddf929554f8887d595c915c715d131ed
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-25 11:11:46 +0530

    Time: 65 ms (64.63%), Space: 79.6 MB (75.81%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findDoll(int[][] envelopes, int n){
+        int max = 1;
+        int[] dp = new int[n];
+        int ans = 0;
+        for (int[] env : envelopes) {
+            int height = env[1];
+            int left = Arrays.binarySearch(dp, 0, ans, height);
+            if (left < 0) left = -left - 1;
+            if (left == ans) ans++;
+            dp[left] = height;
+        }
+        return ans;
+    }
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        return findDoll(envelopes, envelopes.length);
+    }
+}
