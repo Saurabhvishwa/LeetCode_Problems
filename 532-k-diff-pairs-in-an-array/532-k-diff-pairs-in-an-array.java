ca58a89bc53881bddf51aa6948d13d7e915b8fe2
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-26 20:52:33 +0530

    Time: 13 ms (48.37%), Space: 48.3 MB (39.38%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -4,13 +4,13 @@ class Solution {
         int count = 0;
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;++i){
             if(k == 0 && map.containsKey(nums[i])){
                 if(i>1 && nums[i] == nums[i-2]) continue;
-                else count++; continue;
+                else ++count; continue;
             }
             else if(k != 0 && i > 0 && nums[i] != nums[i-1]){
-                if(map.containsKey(nums[i]-k)) count++;
+                if(map.containsKey(nums[i]-k)) ++count;
             }
             map.put(nums[i], 1);
         }
