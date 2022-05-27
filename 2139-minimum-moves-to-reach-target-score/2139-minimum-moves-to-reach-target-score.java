cb03c585d051151c7e013af0b69a1dcd6f0d6057
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-27 10:30:33 +0530

    Time: 1 ms (37.27%), Space: 40.3 MB (67.70%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -3,16 +3,14 @@ class Solution {
         if(maxDoubles == 0) return target-1;
         int count = 0;
         while(target>1){
-            System.out.println(target+" "+maxDoubles+" "+count);
             if(maxDoubles == 0) return count+target-1;
             else if((target&1) == 1){
                 target--;
-                count++;
             }else{
                 target/=2;
                 maxDoubles--;
-                count++;
             }
+            count++;
         }
         return count;
     }
