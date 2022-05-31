c3ae5f8a5eea8e37b5cfbe0a5c81d5954a272bce
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-31 10:34:03 +0530

    Time: 237 ms (29.69%), Space: 95.3 MB (41.15%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -15,6 +15,20 @@ class Solution {
         return count == 0 ? true : false;
     }
     public boolean hasAllCodes(String s, int k) {
+        // int n = s.length();
+        // if(k>n) return false;
+        // int count = 1<<k;
+        // boolean[] visited = new boolean[count];
+        // int num = k>1 ? Integer.parseInt(s.substring(n-k+1),2) << 1 : 0;
+        // for(int i=n-k;i>=0;i--){
+        //     num = ((s.charAt(i)-'0'<<k)+num)>>1;
+        //     if(!visited[num]){
+        //         visited[num] = true;
+        //         --count;
+        //     }
+        //     if(count == 0 ) return true;
+        // }
+        // return count == 0 ? true : false;
         return hasAll(s, k);
     }
 }
