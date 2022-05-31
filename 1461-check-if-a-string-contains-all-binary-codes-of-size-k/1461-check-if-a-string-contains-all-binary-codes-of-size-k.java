06961567e354067e8a314048394b723c096afee1
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-05-31 10:32:05 +0530

    Time: 172 ms (52.61%), Space: 95.2 MB (41.15%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,18 +1,20 @@
 class Solution {
-    public boolean hasAllCodes(String s, int k) {
-        int n = s.length();
+    public boolean hasAll(String s, int k){
+        int n = s.length(), count = 1<<k;
+        Set<String> set = new HashSet<>();
         if(k>n) return false;
-        int count = 1<<k;
-        boolean[] visited = new boolean[count];
-        int num = k>1 ? Integer.parseInt(s.substring(n-k+1),2) << 1 : 0;
-        for(int i=n-k;i>=0;i--){
-            num = ((s.charAt(i)-'0'<<k)+num)>>1;
-            if(!visited[num]){
-                visited[num] = true;
+        String temp = "";
+        for(int i=0;i<n+1-k;i++){
+            temp = s.substring(i,i+k);
+            if(!set.contains(temp)){
+                set.add(temp);
                 --count;
             }
-            if(count == 0 ) return true;
+            if(count == 0) return true;
         }
         return count == 0 ? true : false;
+    }
+    public boolean hasAllCodes(String s, int k) {
+        return hasAll(s, k);
     }
 }
