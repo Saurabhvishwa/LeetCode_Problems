ebfba13612e9c56b994d9694473ae8b3e371671f
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-01 11:27:42 +0530

    Time: 15 ms (44.46%), Space: 75.3 MB (23.45%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -9,21 +9,40 @@
  * }
  */
 class Solution {
+    public ListNode getMid(ListNode head){
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode merge(ListNode left, ListNode right){
+        ListNode cur = new ListNode();
+        ListNode head = cur;
+        while(left != null && right != null){
+            if(left.val < right.val){
+                cur.next = left;
+                left = left.next;
+            }else{
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        if(left != null) cur.next = left;
+        if(right != null) cur.next = right;
+        return head.next;
+    }
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
-        List<Integer> list = new ArrayList<>();
-        ListNode cur = head;
-        while(cur != null){
-            list.add(cur.val);
-            cur = cur.next;
-        }
-        Collections.sort(list);
-        cur = head;
-        int i = 0;
-        while(cur != null){
-            cur.val = list.get(i++);
-            cur = cur.next;
-        }
-        return head;
+        ListNode left = head;
+        ListNode right =  getMid(head);
+        ListNode temp = right.next;
+        right.next = null;
+        right = temp;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
     }
 }
