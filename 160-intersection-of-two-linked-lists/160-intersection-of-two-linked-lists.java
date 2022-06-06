7e9d94e6a68c7f02b1a35fa77fa4a20a3531792e
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-06 09:07:05 +0530

    Time: 1 ms (99.17%), Space: 55.5 MB (38.42%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -10,18 +10,35 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Map<ListNode, Integer> map = new HashMap<>();
-        ListNode cur = headA;
-        while(cur != null){
-            map.put(cur, 1);
-            cur = cur.next;
+    public int getLength(ListNode head){
+        int count = 0;
+        ListNode c = head;
+        while(c != null){
+            count++;
+            c = c.next;
         }
-        cur = headB;
-        while(cur != null){
-            if(map.containsKey(cur)) return cur;
-            cur = cur.next;
+        return count;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        int l1 = getLength(headA), l2 = getLength(headB);
+        if(l1>l2){
+            return getNode(l1-l2, headA, headB);
+        }else{
+            return getNode(l2-l1, headB, headA);
+        }
+    }
+    public ListNode getNode(int d, ListNode headA, ListNode headB){
+        for(int i=0;i<d;i++){
+            if(headA == null) return null;
+            headA = headA.next;
+        }
+        while(headA != null && headB != null){
+            if(headA == headB) return headA;
+            headA = headA.next;
+            headB = headB.next;
         }
         return null;
+        
     }
 }
