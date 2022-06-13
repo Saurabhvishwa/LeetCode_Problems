87419640acb5a539ff2bbce45ca2ad8a2617781e
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-13 10:37:08 +0530

    Time: 14 ms (17.66%), Space: 55.7 MB (71.49%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[2][n];
         int max = 1;
         boolean noOne = true;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int x = matrix[i][j] - '0';
                 if(x == 1 && noOne == true) noOne = false;
-                dp[i][j] = x;
-                if( i == 0 || j == 0  || dp[i][j] == 0) continue;
+                dp[i%2][j] = x;
+                if( i == 0 || j == 0  || dp[i%2][j] == 0) continue;
                 else{
-                    int min = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
-                    dp[i][j] *= min+1;
+                    int min = Math.min(dp[i%2][j-1], Math.min(dp[(i-1)%2][j], dp[(i-1)%2][j-1]));
+                    dp[i%2][j] *= min+1;
                 }
-                if(dp[i][j] > max) max = dp[i][j];
+                if(dp[i%2][j] > max) max = dp[i%2][j];
             }
         }
         return noOne == true ? 0 : max*max;
