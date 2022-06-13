012090447f9678afffb986780bab2ee24760ce61
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-13 09:39:25 +0530

    Time: 12 ms (11.50%), Space: 41.6 MB (99.49%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,21 +1,25 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n+1][triangle.get(n-1).size()];
-        for(int i=1;i<=n;i++){
+        int[][] dp = new int[2][triangle.get(n-1).size()];
+        for(int i=1;i<2;i++){
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
         for(int i=1;i<=n;i++){
             for(int j=0;j<triangle.get(i-1).size();j++){
                 if(j == 0){
-                    dp[i][j] = dp[i-1][j] + triangle.get(i-1).get(j);
-                }else{
-                    dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j-1]) + triangle.get(i-1).get(j);
+                    dp[i%2][j] = dp[(i-1)%2][j] + triangle.get(i-1).get(j);
+                }else if(j == triangle.get(i-1).size()-1){
+                    dp[i%2][j] = dp[(i-1)%2][j-1] + triangle.get(i-1).get(j);
+                }
+                else{
+                    dp[i%2][j] = Math.min(dp[(i-1)%2][j], dp[(i-1)%2][j-1]) + triangle.get(i-1).get(j);
                 }
             }
+            System.out.println(Arrays.toString(dp[(i)%2]));
         }
         int min = Integer.MAX_VALUE;
-        for(int el : dp[n]){
+        for(int el : dp[n%2]){
             if(el<min) min = el;
         }
         return min;
