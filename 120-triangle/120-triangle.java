5128f3176ace28a7f82b0609552a40162cf089ed
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-13 09:40:46 +0530

    Time: 12 ms (11.50%), Space: 44.1 MB (41.06%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=1;i<2;i++){
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
+        int min = Integer.MAX_VALUE;
         for(int i=1;i<=n;i++){
             for(int j=0;j<triangle.get(i-1).size();j++){
                 if(j == 0){
@@ -15,12 +16,10 @@ class Solution {
                 else{
                     dp[i%2][j] = Math.min(dp[(i-1)%2][j], dp[(i-1)%2][j-1]) + triangle.get(i-1).get(j);
                 }
+                if(i == n){
+                    if(dp[i%2][j] < min) min = dp[i%2][j];
+                }
             }
-            System.out.println(Arrays.toString(dp[(i)%2]));
-        }
-        int min = Integer.MAX_VALUE;
-        for(int el : dp[n%2]){
-            if(el<min) min = el;
         }
         return min;
     }
