151c9c5397bc2c5fff701989f42c9b9787ae0403
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-14 11:32:13 +0530

    Time: 23 ms (11.33%), Space: 44.1 MB (72.57%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,32 +1,31 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
+        // int[][] dp = new int[2][n+1];
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i%2][j] = dp[(i-1)%2][j-1] + 1;
+        //         else dp[i%2][j] = Math.max(dp[i%2][j-1], dp[(i-1)%2][j]);
+        //     }
+        // }
+        // return m+n-2*dp[m%2][n];
         int[][] dp = new int[2][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i%2][j] = dp[(i-1)%2][j-1] + 1;
-                else dp[i%2][j] = Math.max(dp[i%2][j-1], dp[(i-1)%2][j]);
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i == 0 && j == 0) continue;
+                if(i == 0) dp[i%2][j] = dp[i%2][j-1]+1;
+                else if(j == 0) dp[i%2][j] = dp[(i-1)%2][j]+1;
+                else{
+                    if(word1.charAt(i-1) == word2.charAt(j-1)){
+                        dp[i%2][j] = dp[(i-1)%2][j-1];
+                    }else{
+                        dp[i%2][j] = Math.min(dp[i%2][j-1], dp[(i-1)%2][j]) + 1;
+                    }
+                }
             }
         }
-        return m+n-2*dp[m%2][n];
+        return dp[m%2][n];
     }
 }
 
 // int m = word1.length(), n = word2.length();
-//         int[][] dp = new int[2][n+1];
-//         dp[0][0] = 0;
-//         for(int i=0;i<=m;i++){
-//             for(int j=0;j<=n;j++){
-//                 if(i == 0 && j == 0) continue;
-//                 if(i == 0) dp[i%2][j] = dp[i%2][j-1]+1;
-//                 else if(j == 0) dp[i%2][j] = dp[(i-1)%2][j]+1;
-//                 else{
-//                     if(word1.charAt(i-1) == word2.charAt(j-1)){
-//                         dp[i%2][j] = dp[(i-1)%2][j-1];
-//                     }else{
-//                         dp[i%2][j] = Math.min(dp[i%2][j-1], dp[(i-1)%2][j]) + 1;
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[m%2][n];
