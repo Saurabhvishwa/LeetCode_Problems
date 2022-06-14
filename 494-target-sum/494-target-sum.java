31829ab4d65f86b252cffabbd61d65880bb1bd03
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-14 13:28:02 +0530

    Time: 7 ms (87.33%), Space: 43.3 MB (43.44%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -13,17 +13,24 @@ class Solution {
     }
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
-        int zeroCount = 0;
-        for(int el : nums){
-            if(el == 0)zeroCount++;
-            sum += el;
+        for(int x : nums)
+            sum += x;
+        if(((sum - target) % 2 == 1) || (target > sum))
+            return 0;
+        
+        int n = nums.length;
+        int s2 = (sum - target)/2;
+        int[][] t = new int[n + 1][s2 + 1];
+        t[0][0] = 1;
+        
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 0; j < s2 + 1; j++) {
+                if(nums[i - 1] <= j)
+                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
         }
-        target = sum-target;
-        if(target < 0 || target%2==1) return 0;
-        findWays(nums, target/2, 0);
-        if(zeroCount>0){
-            count *= Math.pow(2, zeroCount);
-        }
-        return count;
+        return t[n][s2];
     }
 }
