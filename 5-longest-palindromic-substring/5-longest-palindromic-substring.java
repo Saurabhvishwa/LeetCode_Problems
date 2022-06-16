cc4d7fba493b7f7e34efcf63aba6d36fbda044b9
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-16 13:06:34 +0530

    Time: 84 ms (40.79%), Space: 54.3 MB (32.02%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,34 +1,29 @@
 class Solution {
+    public String getLength(String s, int start, int end){
+        if(start<0 || end >= s.length()) return "";
+        while(start>=0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
+        }
+        return s.substring(start+1, end);
+    }
     public String longestPalindrome(String s) {
         if(s.equals("") || s == null){
             return "";
         }
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
+        String temp = "", max ="";
         for(int i=0;i<n;i++){
-            dp[i][i] = true;
-        }
-        int max = 1;
-        int start = 0;
-        for(int i=0;i<n-1;i++){
-            if(s.charAt(i) == s.charAt(i+1)){
-                dp[i][i+1] = true;
-                start = i;
-                max = 2;
+            temp = getLength(s, i, i);
+            if(temp.length()>max.length()){
+                max = temp;
+            }
+            temp =  getLength(s, i, i+1);
+            if(temp.length()>max.length()){
+                max = temp;
             }
         }
-        for(int k=2;k<=n;k++){
-            for(int i=0;i<n-k+1;i++){
-                int j = i+k-1;
-                if(dp[i+1][j-1] && s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = true;
-                    if(k>max){
-                        start = i;
-                        max = k;
-                    }
-                }
-            }
-        }
-        return s.substring(start, start+max);
+        return max;
+        
     }
 }
