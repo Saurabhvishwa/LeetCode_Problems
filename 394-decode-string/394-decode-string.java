7f882ed1fd1898463203c2b00a97b87d30425884
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-22 11:22:01 +0530

    Time: 2 ms (55.35%), Space: 42.4 MB (25.00%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public String decodeString(String s) {
+        String temp = "", top = "", k = "", peek = "", merge = "";
+        int value = 0;
         Stack<String> st = new Stack<>();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) != ']') st.push(String.valueOf(s.charAt(i)));
             else{
-                String temp = "";
-                String top = st.pop();
+                temp = "";
+                top = st.pop();
                 while(!top.equals("[")){
                     temp = top + temp;
                     top = st.pop();
                 }
-                String k = st.pop();
-                String peek = st.isEmpty() ? null : st.peek();
+                k = st.pop();
+                peek = st.isEmpty() ? null : st.peek();
                 while(peek != null && "0".compareTo(peek) <= 0 && "9".compareTo(peek)>=0){
                     k = st.pop() + k;
                     peek = st.isEmpty() ? null : st.peek();
                 }
-                int value = Integer.parseInt(k);
-                String merge = "";
+                value = Integer.parseInt(k);
+                merge = "";
                 while(value>0){
                     merge += temp;
                     value--;
