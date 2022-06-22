a4f518a711ac024b852139de664735df1944f7c3
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-22 20:24:38 +0530

    Time: 4 ms (33.11%), Space: 48.5 MB (23.68%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,18 +1,11 @@
 class Solution {
-    public int getBits(int x){
-        if(x == 0 || x == 1) return x;
-        int count = 0;
-        while(x>0){
-            if(x%2==1) count++;
-            x = x>>1;
-        }
-        return count;
-    }
     public int[] countBits(int n) {
-        int count = 0;
         int[] ans = new int[n+1];
-        for(int i=0;i<=n;i++){
-            ans[i] = getBits(i);
+        if(n == 0) return ans;
+        ans[1] = 1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0) ans[i] = ans[i/2];
+            else ans[i] = ans[i/2] + 1;
         }
         return ans;
     }
