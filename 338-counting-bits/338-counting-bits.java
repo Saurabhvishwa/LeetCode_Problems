c238b6c9ad6a9087af5318a32c5528484362554f
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-22 20:26:28 +0530

    Time: 1 ms (99.95%), Space: 47.7 MB (72.71%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -3,9 +3,11 @@ class Solution {
         int[] ans = new int[n+1];
         if(n == 0) return ans;
         ans[1] = 1;
+        int x = 0;
         for(int i=2;i<=n;i++){
-            if((i&1) == 0) ans[i] = ans[i/2];
-            else ans[i] = ans[i/2] + 1;
+            x = i/2;
+            if((i&1) == 0) ans[i] = ans[x];
+            else ans[i] = ans[x] + 1;
         }
         return ans;
     }
