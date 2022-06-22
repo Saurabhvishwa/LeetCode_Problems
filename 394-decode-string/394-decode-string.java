02449d21bcec5064756d1c38e94e4f3b7270bbdd
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-22 11:25:47 +0530

    Time: 1 ms (87.49%), Space: 42.5 MB (18.86%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public String decodeString(String s) {
-        String temp = "", top = "", k = "", peek = "", merge = "";
+        String temp = "", top = "", k = "", peek = "";
+        StringBuilder merge = new StringBuilder("");
         int value = 0;
         Stack<String> st = new Stack<>();
         for(int i=0;i<s.length();i++){
@@ -19,12 +20,12 @@ class Solution {
                     peek = st.isEmpty() ? null : st.peek();
                 }
                 value = Integer.parseInt(k);
-                merge = "";
+                merge.setLength(0);
                 while(value>0){
-                    merge += temp;
+                    merge.append(temp);
                     value--;
                 }
-                st.push(merge);
+                st.push(merge.toString());
             }
         }
         StringBuilder ans = new StringBuilder("");
