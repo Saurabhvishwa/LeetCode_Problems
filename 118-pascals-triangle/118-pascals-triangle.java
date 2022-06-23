9b3182480cb40001a736b13d18c7429033b36f78
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-23 17:38:29 +0530

    Time: 1 ms (67.08%), Space: 41.6 MB (63.31%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -8,9 +8,7 @@ class Solution {
             List<Integer> row = new ArrayList<>();
             for(int j=0;j<=i;j++){
                 if(j == 0 || j == i) row.add(1);
-                else{
-                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
-                }
+                else row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
             }
             triangle.add(row);
         }
