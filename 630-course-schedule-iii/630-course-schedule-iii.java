1333f0092f1e7d8f3c98d5bac6a91593c667003a
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-23 12:43:47 +0530

    Time: 62 ms (44.44%), Space: 74.6 MB (23.89%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -4,10 +4,7 @@ class Solution {
         Arrays.sort(courses, (a,b) -> a[1] - b[1]);
         for(int i=0,time=0;i<courses.length;i++){
             q.add(courses[i][0]);
-            time += courses[i][0];
-            if(time > courses[i][1]){
-                time -= q.poll();
-            }
+            if((time += courses[i][0]) > courses[i][1]) time -= q.poll();
         }
         return q.size();
     }
