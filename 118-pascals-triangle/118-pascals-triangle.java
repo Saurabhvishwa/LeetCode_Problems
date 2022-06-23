56c11452839643511c6bd008f375b15159f71af2
Saurabh Vishwakarma <66562564+Saurabhvishwa@users.noreply.github.com>
2022-06-23 17:38:01 +0530

    Time: 1 ms (67.08%), Space: 41.8 MB (48.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,24 +1,19 @@
 class Solution {
-    public List<List<Integer>> getPascal(int n){
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        temp.add(1);
-        ans.add(temp);
-        for(int i=2;i<=n;i++){
-            temp = new ArrayList<>();
-            for(int j=0;j<i;j++){
-                if(j == 0 || j == i-1){
-                    temp.add(1);
-                }else{
-                    int value = ans.get(i-2).get(j-1)+ans.get(i-2).get(j);
-                    temp.add(value);
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        triangle.add(first);
+        for(int i=1;i<numRows;i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j == 0 || j == i) row.add(1);
+                else{
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
                 }
             }
-            ans.add(temp);
+            triangle.add(row);
         }
-        return ans;
-    }
-    public List<List<Integer>> generate(int numRows) {
-        return getPascal(numRows);
+        return triangle;
     }
 }
